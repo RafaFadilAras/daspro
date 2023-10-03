71011195575be127142aa2623e94529ffce3c458
rfclz <rafafadilaras@gmail.com>
2023-10-03 07:34:39 +0700

    pemilihan sudut segitiga siku siku

---
+++ daspro-jobsheet6/Pemilihan2Percobaan2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan2 {
+
+    public static void main (String [] args) {
+        Scanner input24 = new Scanner (System.in);
+
+        System.out.println("Masukkan sudut1 : ");
+        int sudut1 = input24.nextInt();
+        System.out.println("Masukkan sudut2 : ");
+        int sudut2 = input24.nextInt();
+        System.out.println("Masukkan sudut3 : ");
+        int sudut3 = input24.nextInt();
+
+        double totalSudut = sudut1 + sudut2 + sudut3;
+
+        if (totalSudut == 180) {
+            if ((sudut1 == 90 ) || (sudut2 == 90 ) || (sudut3 == 90))
+            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else 
+            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+        } else 
+            System.out.println("Bukan segitiga");
+
+
+    }
+
+}
