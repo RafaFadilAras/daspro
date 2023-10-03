3afa75abf4e8981851a6f640eebd114d8ecb88e6
rfclz <rafafadilaras@gmail.com>
2023-10-03 07:14:52 +0700

    tahun 2100

--- daspro-jobsheet6/Pemilihan2Percobaan1.java
+++ daspro-jobsheet6/Pemilihan2Percobaan1.java
@@ -4,15 +4,16 @@ public class Pemilihan2Percobaan1 {
 
     public static void main (String [] args) {
         Scanner input24 = new Scanner (System.in);
-
-        int tahun;
         System.out.println("Masukkan Tahun : ");
-        tahun = input24.nextInt();
+        int tahun = input24.nextInt();
 
-        if ((tahun % 4) == 0) {
+        if ((tahun % 4) == 0)
             if ((tahun % 100) !=0)
+            System.out.println("Tahun Kabisat");
+        else
+            if ((tahun % 400 == 0))
                 System.out.println("Tahun Kabisat");
-        } else
+            else 
             System.out.println("Bukan Tahun Kabisat");
             
     }
