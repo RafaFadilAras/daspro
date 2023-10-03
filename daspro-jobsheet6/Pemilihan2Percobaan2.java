4e6db62358ab2cd3b7860b93f3d8b935e074539b
rfclz <rafafadilaras@gmail.com>
2023-10-03 07:56:46 +0700

    modifikasi pemilihan jenis segitiga

--- daspro-jobsheet6/Pemilihan2Percobaan2.java
+++ daspro-jobsheet6/Pemilihan2Percobaan2.java
@@ -14,15 +14,20 @@ public class Pemilihan2Percobaan2 {
 
         double totalSudut = sudut1 + sudut2 + sudut3;
 
-        if (totalSudut == 180) {
-            if ((sudut1 == 90 ) || (sudut2 == 90 ) || (sudut3 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            else 
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-        } else 
+        if (totalSudut == 180) 
+            if (sudut1==90 || sudut2==90 || sudut3==90)
+                if ((sudut1 == sudut2 ) || (sudut2 == sudut3 ) || (sudut1 == sudut3))
+                    System.out.println("Segitiga tersebut adalah segitiga siku-siku sama kaki");
+                else 
+                    System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+            else if (sudut1==60 && sudut2==60 && sudut3==60)
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if (sudut1==sudut2 || sudut2==sudut3 || sudut1==sudut3)
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            else
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
+        else 
             System.out.println("Bukan segitiga");
-
-
     }
 
 }
