2e053e645559c7f65830b4c46a03acb8169229fa
rfclz <rafafadilaras@gmail.com>
2023-10-10 18:31:21 +0700

    modify forkelipatan

--- daspro-jobsheet7/ForKelipatan24.java
+++ daspro-jobsheet7/ForKelipatan24.java
@@ -10,6 +10,7 @@ public class ForKelipatan24 {
         int kelipatan, jumlah=0, counter=0;
         System.out.println("Masukkan bilangan kelipatan(1-9) : ");
         kelipatan = scan.nextInt();
+        double average; 
 
         for (int i = 1; i<= 50; i++) {
             if (i % kelipatan == 0) {
@@ -17,9 +18,10 @@ public class ForKelipatan24 {
                 counter++;
             }
         }
+        average = jumlah/counter;
         System.out.printf("Banyaknya bilangan %d dari 1-50 adalah %d\n", kelipatan,counter);
         System.out.printf("Total bilangan kelipatan %d dari 1-50 adalah %d\n", kelipatan, jumlah);
-
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1-50 adalah %.2f", kelipatan, average);
     }
-
 }
+
