93000bc124a14607bc94d894d0f08000cdd75788
rfclz <rafafadilaras@gmail.com>
2023-10-28 06:35:03 +0700

    modif arrayNilai status kelulusan

--- daspro-jobsheet9/ArrayNilai24.java
+++ daspro-jobsheet9/ArrayNilai24.java
@@ -4,13 +4,19 @@ public class ArrayNilai24 {
     Scanner sc = new Scanner (System.in);
     int [] nilaiAkhir = new int[10];
     
-    for (int i = 0; i < 10; i++) {
-    System.out.print("Masukkan nilai akhir ke-" + i +" : ");
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+    System.out.print("Masukkan nilai akhir ke-" + i + " : ");
     nilaiAkhir[i] = sc.nextInt();
     }
     
+    String status;
     for (int i = 0; i < 10; i++) {
-    System.out.println("Nilai akhir ke-" +i+ " : " +nilaiAkhir[i]);
-    }
+        if (nilaiAkhir[i] > 70) {
+            status = " Lulus!" ;
+        } else  
+            status = " Tidak Lulus!" ;
+            System.out.println("Mahasiswa ke- " +i+ "" +status);
+        }
     }
 }
+
