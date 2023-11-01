302d90f8a1d52610ac727a02f2ab4a93d87d24d4
rfclz <rafafadilaras@gmail.com>
2023-11-01 19:10:10 +0700

    modify array foreach loop panjangbaris

--- daspro-jobsheet10/Bioskop24.java
+++ daspro-jobsheet10/Bioskop24.java
@@ -15,8 +15,9 @@ public class Bioskop24 {
         System.out.printf("%s \t%s\n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        for (int i =0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" +(i+1)+ ": " +penonton[i].length);
+        for (int j=0; j<penonton.length; j++) {
+            System.out.println("Penonton pada baris ke-" + (j+1) + ":" +String.join (",",penonton[j]));
+        
         }
     }
 }
