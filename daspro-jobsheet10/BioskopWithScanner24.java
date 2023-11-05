0610995df694a3c3cf7bef9b0fe830552ac00d30
rfclz <rafafadilaras@gmail.com>
2023-11-05 19:41:49 +0700

    modify opsi menu percobaan2 array

--- daspro-jobsheet10/BioskopWithScanner24.java
+++ daspro-jobsheet10/BioskopWithScanner24.java
@@ -2,26 +2,51 @@ import java.util.Scanner;
 public class BioskopWithScanner24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int baris, kolom;
+        int menu, baris, kolom;
         String nama, next;
         String [] [] penonton = new String [4][2];
         while (true) {
-            System.out.println("Masukkan nama : ");
-            nama = sc.nextLine();
-            System.out.println("Masukkan baris : ");
-            baris = sc.nextInt();
-            System.out.println("Masukkan kolom");
-            kolom = sc.nextInt();
-            sc.nextLine();
-            
-            penonton[baris-1][kolom-1] = nama;
 
-            System.out.println("Input penonton lainnya? (y/n) : ");
-            next = sc.nextLine();
+            System.out.println("---  Menu  ---");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit\n");
+            System.out.print("Pilih menu : ");
+            menu = sc.nextInt();
+            if (menu==1) {
+                while (true) {
+                    System.out.print("\nMasukkan nama  : ");
+                    nama = sc.next();
+                    System.out.print("Masukkan baris : ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom : ");
+                    kolom = sc.nextInt();
+                    penonton[baris-1][kolom-1] = nama;
+                    System.out.print("Input penonton lainnya? (y/n) : ");
+                    next = sc.next();
+                    if (next.equalsIgnoreCase("n")) {
+                        System.out.println("");
+                        break;
+                    }
+                }
+            }
 
-            if (next.equalsIgnoreCase("n")) {
+            if (menu==2) {
+                System.out.println("");
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.println("Baris ke-"+(i+1));
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        System.out.println("Kolom ke-"+(j+1)+" : "+penonton[i][j] );
+                    }
+                    System.out.println("");
+                }
+            }
+
+            if (menu==3) {
                 break;
             }
+
         }
+
     }
 }
