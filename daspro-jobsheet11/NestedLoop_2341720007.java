0f0e61ef89c365f04f50f90707031f9f60b11cdb
rfclz <rafafadilaras@gmail.com>
2023-11-13 18:38:21 +0700

    modify rerata percobaan5

--- daspro-jobsheet11/NestedLoop_2341720007.java
+++ daspro-jobsheet11/NestedLoop_2341720007.java
@@ -2,19 +2,24 @@ import java.util.Scanner;
 public class NestedLoop_2341720007 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        double[][] temps = new double [5][7];
+        double temps [][] = new double [5][7];
+        double rerata[] = new double[5];
 
         for (int i =0; i<temps.length; i++) {
             System.out.println("Kota ke-" + (i+1));
             for (int j =0; j<temps[0].length; j++) {
-                System.out.print("hari ke-" + (j+1) + ": ");
+                System.out.print("Hari ke-" + (j+1) + ": ");
                 temps [i][j] = sc.nextDouble();
+                rerata[i]+=temps [i][j];
             }
             System.out.println();
         }
         
-        int i =1;
+        for (int i=0; i<rerata.length; i++) {
+            rerata[i]/=7;
+        }
         for (double[] kota : temps) {
+            int i =1;
             System.out.print("Kota ke-" +1+ ": ");
             for (double hari : kota) {
                 System.out.print(hari + " ");
@@ -22,6 +27,9 @@ public class NestedLoop_2341720007 {
             System.out.println();
             i++;
         }
-            }
+        for (int i=0; i < rerata.length; i++) {
+            System.out.println("Rata-rata Kota ke-" + (i+1) + ": " + rerata[i]);
         }
+    }
+}
     
