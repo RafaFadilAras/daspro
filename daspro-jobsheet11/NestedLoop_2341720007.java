8048a5261ea83f612253fff8557ac457d21fb1c5
rfclz <rafafadilaras@gmail.com>
2023-11-13 18:32:05 +0700

    modify array foreach percobaan5

--- daspro-jobsheet11/NestedLoop_2341720007.java
+++ daspro-jobsheet11/NestedLoop_2341720007.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 public class NestedLoop_2341720007 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        double [][] temps = new double [5][7];
+        double[][] temps = new double [5][7];
 
-        for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke - " + (i+1));
-            for (int j = 0; j < temps [0].length; j++) {
-                System.out.println("Hari ke - " + (j+1) + ": ");
+        for (int i =0; i<temps.length; i++) {
+            System.out.println("Kota ke-" + (i+1));
+            for (int j =0; j<temps[0].length; j++) {
+                System.out.print("hari ke-" + (j+1) + ": ");
                 temps [i][j] = sc.nextDouble();
             }
             System.out.println();
         }
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke - " + (i+1)+ ": ");
-            for (int j =0; j < temps [0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+        
+        int i =1;
+        for (double[] kota : temps) {
+            System.out.print("Kota ke-" +1+ ": ");
+            for (double hari : kota) {
+                System.out.print(hari + " ");
             }
             System.out.println();
+            i++;
         }
-    }
-}
+            }
+        }
+    
