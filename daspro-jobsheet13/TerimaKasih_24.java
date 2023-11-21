087d3e17b78f59acaa97723cfaecb8287b1400b7
rfclz <rafafadilaras@gmail.com>
2023-11-21 17:27:54 +0700

    modify percobaan1

--- daspro-jobsheet13/TerimaKasih_24.java
+++ daspro-jobsheet13/TerimaKasih_24.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
 public class TerimaKasih_24 {
-    public static void UcapanTerimaKasih() {
+    public static void ucapanTerimaKasih() {
         System.out.println("Thank You for being the best teacher in the world.\n"+
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+    public static void ucapanTambahan (String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
-        UcapanTerimaKasih();
+        ucapanTerimaKasih();
+        String ucapan = "Terimakasih Pak.. Bu.. semoga sehat selalu";
+        ucapanTambahan(ucapan);
     }
 }
