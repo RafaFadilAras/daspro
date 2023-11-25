deeaddd0d84770b3d01cbecd63220c13fd8fe5e4
rfclz <rafafadilaras@gmail.com>
2023-11-25 08:30:47 +0700

    modify no3 percobaan4

--- daspro-jobsheet13/UcapanTerimaKasih_24.java
+++ daspro-jobsheet13/UcapanTerimaKasih_24.java
@@ -12,7 +12,12 @@ public class UcapanTerimaKasih_24 {
         System.out.println("Thank you " +nama+ " for being the best teacher in the world.\n" +
         "You inspired in me a love for learning and made me feel like I could ask you anything.");
     }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = ".";
+        UcapanTambahan(ucapan);
     }
 }
